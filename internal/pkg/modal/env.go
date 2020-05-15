2295fdf1289c9e5990fa3b9fddf136a74acebef0
gatiwari <gaurav.tiwari1@blackrock.com>
2020-05-15 17:00:56 -0400

    Hard coding the region for now

--- internal/pkg/modal/env.go
+++ internal/pkg/modal/env.go
@@ -10,12 +10,13 @@ import (
 // GetDynamoDBSession : To return the dynamoDB session based on environment
 func GetDynamoDBSession() (db *dynamo.DB) {
 	var awsSession = session.Must(session.NewSession())
-	var config = aws.Config {
-	}
+	var config = aws.Config{}
 	localDev := os.Getenv("LocalDevelopment")
 	if len(localDev) != 0 {
 		config.Endpoint = aws.String("http://localhost:8000")
-		config.Region= aws.String("us-west-2")
+		config.Region = aws.String("us-west-2")
+	} else {
+		config.Region = aws.String("us-east-2")
 	}
 	db = dynamo.New(awsSession, &config)
 	return
